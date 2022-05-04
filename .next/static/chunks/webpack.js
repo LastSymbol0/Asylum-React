f1036509436addd71083e64d383777ffbf9a96b5
YaremenkoM <mariayaremenko13@gmail.com>
2022-05-04 22:26:19 +0100

    add materials

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b59a461ec18615b"; }
+/******/ 		__webpack_require__.h = function() { return "f54674973c6014bd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.921d9a02a799122d.hot-update.js
+++ .next/static/webpack/webpack.921d9a02a799122d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "82590b8e7af0864c"; }
+/******/ 	__webpack_require__.h = function() { return "f54674973c6014bd"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
