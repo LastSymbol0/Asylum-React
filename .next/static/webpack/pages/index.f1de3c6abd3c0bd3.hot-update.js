f9db1fc7b005f6cfad6675d788767f7c393929d2
YaremenkoM <mariayaremenko13@gmail.com>
2022-05-25 00:17:30 +0100

    update roadmap elements

---
+++ .next/static/webpack/pages/index.f1de3c6abd3c0bd3.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./pages-content/home/roadmapScreen/assets/circle.png":
+/*!************************************************************!*\
+  !*** ./pages-content/home/roadmapScreen/assets/circle.png ***!
+  \************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/circle.aebea603.png\",\"height\":101,\"width\":101,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcircle.aebea603.png&w=8&q=70\"});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy1jb250ZW50L2hvbWUvcm9hZG1hcFNjcmVlbi9hc3NldHMvY2lyY2xlLnBuZy5qcyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsQ0FBQyxtS0FBbUsiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcGFnZXMtY29udGVudC9ob21lL3JvYWRtYXBTY3JlZW4vYXNzZXRzL2NpcmNsZS5wbmc/ZGRkMyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XCJzcmNcIjpcIi9fbmV4dC9zdGF0aWMvbWVkaWEvY2lyY2xlLmFlYmVhNjAzLnBuZ1wiLFwiaGVpZ2h0XCI6MTAxLFwid2lkdGhcIjoxMDEsXCJibHVyRGF0YVVSTFwiOlwiL19uZXh0L2ltYWdlP3VybD0lMkZfbmV4dCUyRnN0YXRpYyUyRm1lZGlhJTJGY2lyY2xlLmFlYmVhNjAzLnBuZyZ3PTgmcT03MFwifTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages-content/home/roadmapScreen/assets/circle.png\n");
+
+/***/ })
+
+});
