d5949fe8c236d08a0ee86c94921a3df02ff74c1f
bb <oleh.kvsk@gmail.com>
2022-07-20 16:24:23 +0300

    Google Tag Manager redone

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../assets/index.scss'
 
 import { useEffect } from 'react'
 import renderer from '../utils/renderer'
-import TagManager from 'react-gtm-module'
 
 import Header from '../components/header/header'
 import ScrollTop from '../components/scrollTop/scrollTop'
@@ -11,6 +10,7 @@ import smoothscroll from 'smoothscroll-polyfill'
 
 import { useRouter } from 'next/router'
 import Head from 'next/head'
+import Script from 'next/script'
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter()
@@ -27,20 +27,32 @@ const MyApp = ({ Component, pageProps }) => {
     // renderer.setToRender({handler: () => console.log(renderer.getRendering())})
   })
 
-  useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-N4V47KJ' })
-  }, [])
-
   return (
     <>
       <Head>
         <link rel='icon' href='/asylum.png' />
       </Head>
+      <Script
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N4V47KJ');`,
+        }}
+      ></Script>
       <Header />
       <div className='scrollContainer'>
         <Component {...pageProps} />
       </div>
       <ScrollTop />
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N4V47KJ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      ></noscript>
     </>
   )
 }
